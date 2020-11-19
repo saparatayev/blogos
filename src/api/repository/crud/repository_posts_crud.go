80f58154938b8670edd8584dada6e21c4195c496
saparatayev <sopa.97@mail.ru>
2020-11-19 17:40:20 +0500

    feat: findById func

--- src/api/repository/crud/repository_posts_crud.go
+++ src/api/repository/crud/repository_posts_crud.go
@@ -3,6 +3,7 @@ package crud
 import (
 	"blogos/src/api/models"
 	"blogos/src/api/utils/channels"
+	"time"
 
 	"github.com/jinzhu/gorm"
 )
@@ -78,4 +79,66 @@ func (r *repositoryPostsCRUD) FindAll() ([]models.Post, error) {
 	}
 
 	return nil, err
+}
+
+func (r *repositoryPostsCRUD) FindById(pid uint64) (models.Post, error) {
+	var err error
+
+	post := models.Post{}
+
+	done := make(chan bool)
+
+	go func(ch chan<- bool) {
+		defer close(ch)
+
+		err = r.db.Debug().Model(&models.Post{}).Where("id = ?", pid).Take(&post).Error
+		if err != nil {
+			ch <- false
+
+			return
+		}
+
+		if post.ID != 0 {
+			err = r.db.Debug().Model(&models.User{}).Where("id = ?", post.AuthorID).Take(&post.Author).Error
+			if err != nil {
+				ch <- false
+
+				return
+			}
+		}
+
+		ch <- true
+	}(done)
+
+	if channels.OK(done) {
+		return post, nil
+	}
+
+	return models.Post{}, err
+}
+
+func (r *repositoryPostsCRUD) FindById(pid uint64, post models.Post) (int64, error) {
+	var rs *gorm.DB
+
+	done := make(chan bool)
+
+	go func(ch chan<- bool) {
+		defer close(ch)
+
+		rs = r.db.Debug().Model(&models.Post{}).Where("id = ?", pid).Take(&models.Post{}).UpdateColumns(
+			map[string]interface{}{
+				"title":      post.Title,
+				"content":    post.Content,
+				"updated_at": time.Now(),
+			},
+		)
+
+		ch <- true
+	}(done)
+
+	if channels.OK(done) {
+		return post, nil
+	}
+
+	return models.Post{}, err
 }
